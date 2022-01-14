8b3aa12b38a2fabec9aef5d7d288d9d2ea9c9419
Álvaro Piza <59285436+alvarotoledopiza@users.noreply.github.com>
2022-01-14 01:28:46 +0000

    Initial commit

---
+++ introducao.py
@@ -0,0 +1,16 @@
+#My first Python program
+'''
+Álvaro Piza
+UAM Calculator
+My first code in Py, this is a quick comment.
+'''
+
+nome = input('Insira seu nome: ')
+idade = input('Insira sua idade')
+idade = integer(idade)
+cidade = 'São José dos Campos'
+print('O ' + nome + ' tem ' + idade + ' anos de idade e mora em ' + cidade + '.')
+
+
+
+
